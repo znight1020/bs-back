092a8d4157e9c40275fd921fe78b22e5f806e616
이현수 <znight1020@naver.com>
2025-04-25 20:24:16 +0900

    feat: 상태 코드 정의

--- src/main/java/toy/bookswap/web/member/controller/MemberController.java
+++ src/main/java/toy/bookswap/web/member/controller/MemberController.java
@@ -1,9 +1,11 @@
 package toy.bookswap.web.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import toy.bookswap.domain.member.service.MemberService;
 import toy.bookswap.web.common.CommonResponse;
@@ -18,6 +20,7 @@ public class MemberController {
   private final MemberService memberService;
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public CommonResponse<ResponseSymbol> singup(@RequestBody SignupRequest request) {
     memberService.signupProcess(request.toCommand());
     return new CommonResponse<>(true, ResponseSymbol.CREATED);
