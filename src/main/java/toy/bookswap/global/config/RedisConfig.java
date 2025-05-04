5a3ad06c6a6063426d085abf04e4fcfcd8791953
이현수 <znight1020@naver.com>
2025-05-04 23:36:32 +0900

    refactor: redis 설정 매핑

--- src/main/java/toy/bookswap/global/config/RedisConfig.java
+++ src/main/java/toy/bookswap/global/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package toy.bookswap.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,9 +12,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+  @Value("${spring.data.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.data.redis.port}")
+  private int redisPort;
+
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
+    RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+    redisConfiguration.setHostName(redisHost);
+    redisConfiguration.setPort(redisPort);
+    return new LettuceConnectionFactory(redisConfiguration);
   }
 
   @Bean
