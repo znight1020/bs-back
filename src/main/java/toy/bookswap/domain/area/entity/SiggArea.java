dcfb125745e9664dfa188ca40bf57eb76a3608a3
이현수 <znight1020@naver.com>
2025-05-09 03:13:40 +0900

    feat: Sido, Sigg, Emd 행정구역 Entity 정의

---
+++ src/main/java/toy/bookswap/domain/area/entity/SiggArea.java
@@ -0,0 +1,36 @@
+package toy.bookswap.domain.area.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "sigg_areas")
+public class SiggArea {
+
+  @Id
+  private Integer id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "sido_area_id", nullable = false)
+  private SidoArea sidoArea;
+
+  @Column(name = "adm_code", length = 5, nullable = false)
+  private String admCode;
+
+  @Column(nullable = false, length = 50)
+  private String name;
+}
---
+++ src/main/java/toy/bookswap/domain/member/entity/activity/ActivityAreaId.java
@@ -0,0 +1,23 @@
+package toy.bookswap.domain.member.entity.activity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class ActivityAreaId implements Serializable {
+
+  @Column
+  private Long memberId;
+
+  @Column
+  private Integer emdAreaId;
+}
