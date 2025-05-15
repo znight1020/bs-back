6a4c69411bece8a0473c9c902b8a500c4747972d
이현수 <znight1020@naver.com>
2025-05-15 18:50:34 +0900

    feat: 활성화 된 프로필 cookie samesite 값 부여

---
+++ src/main/java/com/bob/global/props/CookieProps.java
@@ -0,0 +1,18 @@
+package com.bob.global.props;
+
+import com.bob.global.utils.CookieUtils;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@ConfigurationProperties(prefix = "cookie")
+@Component
+public class CookieProps {
+  private String sameSite;
+
+  public void setSameSite(String sameSite) {
+    this.sameSite = sameSite;
+    CookieUtils.setSameSite(sameSite);
+  }
+}
