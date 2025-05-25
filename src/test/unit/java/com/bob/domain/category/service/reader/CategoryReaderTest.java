4c2bc006be3bef545dc94d74600959e075f0fa01
이현수 <znight1020@naver.com>
2025-05-26 02:03:47 +0900

    test: 카테고리 조회 기능 단위 테스트

---
+++ src/test/unit/java/com/bob/domain/category/service/reader/CategoryReaderTest.java
@@ -0,0 +1,59 @@
+package com.bob.domain.category.service.reader;
+
+import static com.bob.support.fixture.domain.CategoryFixture.defaultCategory;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import com.bob.domain.category.entity.Category;
+import com.bob.domain.category.repository.CategoryRepository;
+import com.bob.global.exception.exceptions.ApplicationException;
+import com.bob.global.exception.response.ApplicationError;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryReaderTest {
+
+  @InjectMocks
+  private CategoryReader categoryReader;
+
+  @Mock
+  private CategoryRepository categoryRepository;
+
+  @Test
+  @DisplayName("카테고리 ID로 조회 - 존재하는 경우")
+  void 카테고리ID로_조회하면_존재하는_경우_정상_반환한다() {
+    // given
+    Category category = defaultCategory();
+    given(categoryRepository.findById(category.getId())).willReturn(Optional.of(category));
+
+    // when
+    Category result = categoryReader.readCategoryById(category.getId());
+
+    // then
+    assertThat(result).isEqualTo(category);
+    verify(categoryRepository).findById(category.getId());
+  }
+
+  @Test
+  @DisplayName("카테고리 ID로 조회 - 존재하지 않는 경우 예외 발생")
+  void 카테고리ID로_조회하면_존재하지_않으면_예외를_던진다() {
+    // given
+    Long categoryId = -1L;
+    given(categoryRepository.findById(categoryId)).willReturn(Optional.empty());
+
+    // when & then
+    assertThatThrownBy(() -> categoryReader.readCategoryById(categoryId))
+        .isInstanceOf(ApplicationException.class)
+        .hasMessage(ApplicationError.UN_SUPPORTED_CATEGORY.getMessage());
+
+    verify(categoryRepository).findById(categoryId);
+  }
+}
---
+++ src/test/unit/java/com/bob/support/fixture/domain/CategoryFixture.java
@@ -0,0 +1,16 @@
+package com.bob.support.fixture.domain;
+
+import com.bob.domain.category.entity.Category;
+
+public class CategoryFixture {
+
+  public static final Long DEFAULT_CATEGORY_ID = 1L;
+  public static final String DEFAULT_CATEGORY_NAME = "경제/경영";
+
+  public static Category defaultCategory() {
+    return Category.builder()
+        .id(DEFAULT_CATEGORY_ID)
+        .name(DEFAULT_CATEGORY_NAME)
+        .build();
+  }
+}
